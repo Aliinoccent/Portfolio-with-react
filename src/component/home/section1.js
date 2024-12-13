50290fadf83b6b36cc81954f99514da55386a565
unknown <aliinoccent786@gmail.com>
2024-12-13 10:35:44 -0800

    adjustment

--- src/component/home/section1.js
+++ src/component/home/section1.js
@@ -1,84 +1,80 @@
 import { useRef, useEffect } from 'react';
 import darkcomputer from './img/darkcomputer.jpg';
-import { gsap, Power3,Power2 } from 'gsap';
+import { gsap, Power3, Power2 } from 'gsap';
 
 const Section1 = () => {
     const firstText = useRef(null);
     const secondText = useRef(null);
     const thirdText = useRef(null);
-    const forthText=useRef(null)
-    const image=useRef(null)
+    const forthText = useRef(null)
+    const image = useRef(null)
     useEffect(() => {
         const tl = gsap.timeline(); // Create a single timeline
 
         tl.fromTo(
             firstText.current,
             { opacity: 0, y: 20 },
-            { opacity: 1, y: -12, duration: 1, ease: Power3.easeOut }
+            { opacity: 1, y: -12, duration: 1, ease: Power2.easeOut }
         )
             .fromTo(
                 secondText.current,
                 { opacity: 0, y: 20 },
-                { opacity: 1, y: -12, duration: 1, ease: Power3.easeOut },
-                "-=0.5" // Start 0.5 seconds earlier for overlap
-            )
-            .fromTo(
-                forthText.current,{
-                    opacity:0,y:20},
-                {opacity:1,y:-12,duration:1,ease:Power3.easeOut},
-                "-=0.5"
+                { opacity: 1, y: 10, duration: 1, ease: Power2.easeOut },
+
             )
             .fromTo(
                 thirdText.current,
                 { opacity: 0, y: 20 },
-                { opacity: 1, y: -12, duration: 1, ease: Power3.easeOut },
-                "-=0.5"
+                { opacity: 1, y: 10, duration: 0, ease: Power2.easeOut },
+
+            )
+            .fromTo(
+                forthText.current,
+                { opacity: 0, y: 20 },
+                { opacity: 1, y: 10, duration: 0, ease: Power2.easeOut }
             );
-            gsap.fromTo(image.current,{
-            duration:2,
-            scale:0.3,
-            opacity:0,
-            
-        },
-        {
-            opacity:1,
-            duration:1,
-            scale:1,
-            ease:'back.inOut'
-        }
-    )
+
+        gsap.fromTo(
+            image.current,
+            { opacity: 0, scale: 0 },
+            { opacity: 1, scale: 1, duration: 1, ease: "back.inOut" }
+        );
     }, []);
 
-    return (
-        <div className="container-fluid d-flex width-50" style={{ flexDirection: 'row' , boxSizing:'border-box',
-            margin:0,
-            padding:0}}>
-            <div
-                className="left bg-dark col-6"
-                style={{
-                    
-                    display: 'flex',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                     
-                }}
-            >
-                <div className="h6 text-warning" ref={firstText}>
-                    <span className="">hi i am!</span>
-                </div>
-                <div className="h1 text-white" >
-                    <span className=""ref={secondText}>Abdul</span>
-                    <br />
-                    <span ref={forthText}>Rehman</span>
-                </div>
-                <span className="text-white" ref={thirdText}>
-                    front developer
-                </span>
-            </div>
 
-            <div className="right col-6">
-                <img ref={image} className="img-fluid" src={darkcomputer} alt="not img" />
+    return (
+        <div className="container-fluid " style={{ boxSizing: 'border-box', margin: 0, padding: 0 }}>
+            <div className='row'>
+                <div
+                    className="left bg-dark col-lg-6 col-sm-12 col-md-12"
+                    style={{
+
+                        display: 'flex',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        margin:0,
+                        padding:0,
+                        boxSizing:'border-box',
+
+                    }}
+                >
+                    <div className="h6 text-warning" ref={firstText}>
+                        <span className="">hi i am!</span>
+                    </div>
+                    <div className="h1 text-white" >
+                        <span className="" ref={secondText}>Abdul</span>
+                        <br />
+                        <span className="" ref={thirdText} style={{ opacity: 0 }}>Rehman</span>
+                    </div>
+                    <span className="text-white" ref={forthText}>
+                        front developer
+                    </span>
+                </div>
+
+                <div className="right col-lg-6 col-sm-12 col-md-12 p-0">
+                    <img ref={image} className="img-fluid" src={darkcomputer} alt="not img" />
+                </div>
             </div>
         </div>
     );
